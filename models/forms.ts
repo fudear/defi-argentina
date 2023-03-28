647775322bf29591269bbb1be4ffab1db81c70a9
Brian Sasbon <bsasbon@neopower.digital>
2023-03-27 21:45:50 -0300

    feat: add forms links

--- models/forms.ts
+++ models/forms.ts
@@ -1,5 +1,5 @@
 export const FORMS = {
-  VOLUNTEER: "https://google.com",
-  ORGANIZATION: "https://google.com",
-  HELP: "https://google.com",
+  VOLUNTEER: "https://forms.gle/E5DH8FrVeVLqKqQ38",
+  ORGANIZATION: "https://forms.gle/WT3JLQ1nazBBWxTE8",
+  HELP: "https://forms.gle/GfzdQhVVtF4Y9j4b6",
 };
